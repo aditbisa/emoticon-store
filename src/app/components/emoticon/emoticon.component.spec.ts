a3274329b09fe2d9420d2eeeb231f0fd5363e28a
Aditya Ghaffar <adit.bisa@gmail.com>
2022-11-28 09:19:24 +0700

    feat: purchase emoticon

--- src/app/components/emoticon/emoticon.component.spec.ts
+++ src/app/components/emoticon/emoticon.component.spec.ts
@@ -1,16 +1,21 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
-
+import { ContractService } from '@core/services';
 import { EmoticonComponent } from './emoticon.component';
 
 describe('EmoticonComponent', () => {
   let component: EmoticonComponent;
   let fixture: ComponentFixture<EmoticonComponent>;
+  let contractServiceSpy: jasmine.SpyObj<ContractService>;
 
   beforeEach(waitForAsync(() => {
+    contractServiceSpy = jasmine.createSpyObj('ContractService', ['purchase']);
+    contractServiceSpy.purchase.and.stub();
+
     TestBed.configureTestingModule({
-      declarations: [ EmoticonComponent ],
-      imports: [IonicModule.forRoot()]
+      providers: [{ provide: ContractService, useValue: contractServiceSpy }],
+      declarations: [EmoticonComponent],
+      imports: [IonicModule.forRoot()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(EmoticonComponent);
--- src/app/components/emoticon/emoticon.component.ts
+++ src/app/components/emoticon/emoticon.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Emoticon } from '@core/schema';
+import { environment as env } from '@env';
+import { ContractService } from '@core/services';
 
 @Component({
   selector: 'app-emoticon',
@@ -9,7 +11,19 @@ import { Emoticon } from '@core/schema';
 export class EmoticonComponent implements OnInit {
   @Input() emoticon!: Emoticon;
 
-  constructor() {}
+  constructor(
+    private contractService: ContractService,
+  ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.contractService.Purchase$.subscribe((data) => {
+      if (data.codePoint == this.emoticon.codePoint) {
+        this.emoticon.sold = true;
+      }
+    });
+  }
+
+  public async purchase() {
+    await this.contractService.purchase(env.testBuyerAddress, this.emoticon.codePoint, "1000000");
+  }
 }
