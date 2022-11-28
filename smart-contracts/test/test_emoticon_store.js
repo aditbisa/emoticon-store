59d8f9fc016cce106f5a6f911ac246681a7ecef6
Aditya Ghaffar <adit.bisa@gmail.com>
2022-11-28 13:58:57 +0700

    test: update test with owner address setup

--- smart-contracts/test/test_emoticon_store.js
+++ smart-contracts/test/test_emoticon_store.js
@@ -2,39 +2,38 @@ const TestEmoticonStore = artifacts.require("EmoticonStore");
 const BN = web3.utils.BN;
 
 const getBalance = async (acc) => new BN(await web3.eth.getBalance(acc));
+const { OWNER_ADDRESS } = process.env;
 
 /*
- * uncomment accounts to access the test accounts made available by the
- * Ethereum client
  * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
  */
 contract("Test Emoticon Store Contract", function (accounts) {
   let emoticonStore;
 
   it("should be deployed", async function () {
-    emoticonStore = await TestEmoticonStore.deployed({ from: accounts[0] });
+    emoticonStore = await TestEmoticonStore.deployed();
     return assert.isTrue(true);
   });
 
   it("should have correct owner", async function () {
     const owner = await emoticonStore.getStoreOwner();
-    return assert.equal(owner, accounts[0]);
+    return assert.equal(owner, OWNER_ADDRESS);
   });
 
   it("should process a purchase", async function () {
-    const sellerBalanceBefore = await getBalance(accounts[0]);
-    const buyerBalanceBefore = await  getBalance(accounts[1]);
+    const sellerBalanceBefore = await getBalance(OWNER_ADDRESS);
+    const buyerBalanceBefore = await  getBalance(accounts[0]);
 
-    const result = await emoticonStore.purchase(12345, { from: accounts[1], value: 10 });
+    const result = await emoticonStore.purchase(12345, { from: accounts[0], value: 10 });
 
-    const sellerBalanceAfter = await getBalance(accounts[0]);
-    const buyerBalanceAfter = await getBalance(accounts[1]);
+    const sellerBalanceAfter = await getBalance(OWNER_ADDRESS);
+    const buyerBalanceAfter = await getBalance(accounts[0]);
 
     // Check event
     const event = result.logs.find(e => e.event == "Purchase");
     assert.isObject(event);
     assert.equal(event.args.codePoint, 12345);
-    assert.equal(event.args.buyer, accounts[1]);
+    assert.equal(event.args.buyer, accounts[0]);
 
     // Check balance
     assert.isTrue(sellerBalanceAfter.gt(sellerBalanceBefore));
@@ -49,7 +48,7 @@ contract("Test Emoticon Store Contract", function (accounts) {
 
   it("should return the buyer", async function () {
     const buyer = await emoticonStore.getBuyer(12345);
-    return assert.equal(buyer, accounts[1]);
+    return assert.equal(buyer, accounts[0]);
   });
 
   it("cannot purchase an already purchased item", async function () {
