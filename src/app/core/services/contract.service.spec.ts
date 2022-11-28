e3a86c36e49a8e634261318b3b4c9d5071d24ebc
Aditya Ghaffar <adit.bisa@gmail.com>
2022-11-28 17:34:07 +0700

    test: setup provider

--- src/app/core/services/contract.service.spec.ts
+++ src/app/core/services/contract.service.spec.ts
@@ -1,11 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 import { environment as env } from '@env';
-import Web3 from 'web3';
 import { ContractService } from './contract.service';
+import Web3 from 'web3';
 import { Web3Service } from './web3.service';
 
+// Need local network! And don't forget to migrate the contract and update the address in env.
 const ownerAccount = env.testOwnerAddress;
 const buyerAccount = env.testBuyerAddress;
+const provider = env.testProvider;
 const codePoint = parseInt(String(Date.now()).slice(-4));
 
 describe('ContractService', () => {
@@ -15,9 +17,7 @@ describe('ContractService', () => {
   let web3: Web3;
 
   beforeEach(async () => {
-    web3Provider = new Web3.providers.HttpProvider(
-      'http://172.30.144.1:7545' //http://localhost:7545'
-    );
+    web3Provider = new Web3.providers.HttpProvider(provider);
     web3 = new Web3(web3Provider);
     const contract = new web3.eth.Contract(
       env.contractAbi,
